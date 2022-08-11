cee2ce1f487fe2cc65e2634e5066052b5bb0aacf
IvanIvanson <ghalut1977@gmail.com>
2022-08-11 10:29:52 +0300

    lesson3

--- countdown.js
+++
@@ -1,9 +0,0 @@
-let date = new Date();
-let targetDate = new Date("January 1, 2023 00:01:00");
-
-let day = Math.floor((targetDate - date) / 1000 / 60 / 60 / 24) - 1;
-let hours = 23 - date.getHours();
-let min = 60 - date.getMinutes();
-console.log(
-  "Until the new Year: " + day + " days, " + hours + " hourse, " + min + " minutes"
-);
